421a4401206dc5be26e97f0ad2435ed60d3856c5
HeMinzhang <loongsking@gmail.com>
2018-10-31 09:14:12 +0800

    Upload Project

---
+++ src/HeXuShi.Extensions.NoWWW/NoWWWPolicyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using HeXuShi.Extensions.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace HeXuShi.Extensions
+{
+    public static class NoWWWPolicyExtensions
+    {
+        public static IApplicationBuilder UseNoWWW(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+            app.UseMiddleware<NoWWWMiddleware>();
+            return app;
+        }
+    }
+}
