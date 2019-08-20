c151029298edde19014e2e662efa21131dea4f75
赫敏璋 <loongsking@hotmail.com>
2019-08-20 10:28:37 +0800

    Completed the ipv4 search demo

--- demo/IpSearchDemo/Program.cs
+++ demo/IpSearchDemo/Program.cs
@@ -27,8 +27,8 @@ namespace IpSearchDemo
         }
         static async Task ReadDataBase()
         {
-            const string ipv4File = "ipv4.cn.zone.txt";
-            const string ipv6File = "ipv6.cn.zone.txt";
+            const string ipv4File = "IP2LOCATION-LITE-DB1.CSV";
+            const string ipv6File = "IP2LOCATION-LITE-DB1.IPV6.CSV";
             string ipv4Text;
             string ipv6Text;
             using (var file = File.OpenText(ipv4File))
@@ -60,32 +60,41 @@ namespace IpSearchDemo
         }
         static void readIpv4Range(string text)
         {
-            List<IPAdressRange> ipRanges = new List<IPAdressRange>();
-            var table = text.Split('\n');
+            //List<IPAdressRange> ipRanges = new List<IPAdressRange>();
+            var table = text.Split("\r\n");
+
             foreach (var item in table)
-            {
-                if (item.Length < 5)
-                    continue;
-                var keyVal = item.Split('/');
-                if (keyVal.Length != 2)
-                    throw new InvalidOperationException("ipv4 error ip range");
-                var ipRange = new IPAdressRange
-                {
-                    Min = keyVal[0]
-                };
-                var maxIp = keyVal[0];
-                var index = maxIp.LastIndexOf(".");
-                maxIp = maxIp.Remove(index, maxIp.Length - index);
-                ipRange.Max = maxIp + "." + keyVal[1];
-                ipRanges.Add(ipRange);
-            }
-            foreach(var item in ipRanges)
-            {
-                var rangeBytes = new IpAddressByte();
-                rangeBytes.Min = Ipv4ToByte(item.Min);
-                rangeBytes.Max = Ipv4ToByte(item.Max);
-                ipv4Ranges.Add(rangeBytes);
-            }
+           {
+            if (item.Length < 5)
+                   continue;
+            var keyVal = item.Split(',');
+             if (keyVal.Length < 4)
+                  throw new InvalidOperationException("ipv4 error ip range");
+
+             if(keyVal[2] == "\"CN\"")
+             {
+                    var rangeBytes = new IpAddressByte();
+                    rangeBytes.Min = Convert.ToUInt32(keyVal[0].Replace("\"", ""));
+                    rangeBytes.Max = Convert.ToUInt32(keyVal[1].Replace("\"", ""));
+                    ipv4Ranges.Add(rangeBytes);
+                }
+            //    var ipRange = new IPAdressRange
+            //    {
+            //        Min = keyVal[0]
+            //    };
+            //    var maxIp = keyVal[0];
+            //    var index = maxIp.LastIndexOf(".");
+            //    maxIp = maxIp.Remove(index, maxIp.Length - index);
+            //    ipRange.Max = maxIp + "." + keyVal[1];
+            //    ipRanges.Add(ipRange);
+           }
+            //foreach(var item in ipRanges)
+            //{
+            //    var rangeBytes = new IpAddressByte();
+            //    rangeBytes.Min = Ipv4ToByte(item.Min);
+            //    rangeBytes.Max = Ipv4ToByte(item.Max);
+            //    ipv4Ranges.Add(rangeBytes);
+            //}
         }
         static void readIpv6Range(string text)
         {
